8df0281a18844052e1dae8369b1345b84760381c
Shafiur Rahman Rad <shafiur008@gmail.com>
2024-12-31 00:26:08 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -13,9 +13,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
 	die("connection failed: " . $conn->connect_error);
     }
-else{
-	mysqli_select_db($conn, $dbname);
-	echo "Connection successful";
-	}
+// else{
+// 	mysqli_select_db($conn, $dbname);
+// 	echo "Connection successful";
+// 	}
 
 ?>
