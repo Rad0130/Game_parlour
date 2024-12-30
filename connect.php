ad2489b141f95044ebd0444a67d7828effb20927
Shafiur Rahman Rad <shafiur008@gmail.com>
2024-12-30 20:36:40 +0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="game_parlour";
+
+//creating connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if ($conn->connect_error){
+	die("connection failed: " . $conn->connect_error);
+    }
+else{
+	mysqli_select_db($conn, $dbname);
+	echo "Connection successful";
+	}
+
+?>
